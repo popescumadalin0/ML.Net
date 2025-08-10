6f437c54257346ca77d2ffa1701aebd3b558135e
madalinpopescu <dizzyforester@gmail.com>
2025-08-10 10:57:23 +0300

    Initial commit

---
+++ ML.Net/Models/Activity.cs
@@ -0,0 +1,14 @@
+namespace ML.Net.Models;
+
+public class Activity
+{
+    public string Name { get; set; }
+    
+    public bool IsPending { get; set; }
+    
+    public string LastUpdate { get; set; }
+    
+    public string Icon { get; set; }
+    
+    public string Status { get; set; } 
+}
